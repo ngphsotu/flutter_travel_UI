e8ecc19ffd42345aa4e76c18c145b57847b7c494
Nguyen Phuc Son Tung <52483128+ngphsotu@users.noreply.github.com>
2022-07-17 00:35:13 +0700

    welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../pages/welcome_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const WelcomePage(),
     );
   }
 }
