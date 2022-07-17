6e80d745bb42adeef3c177a1bc7d7a8005f40008
Nguyen Phuc Son Tung <52483128+ngphsotu@users.noreply.github.com>
2022-07-18 01:42:30 +0700

    fix bug

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -38,7 +38,7 @@ class AppButtons extends StatelessWidget {
           ? Center(
               child: AppText(
               text: text!,
-              color: Colors.black,
+              color: color,
             ))
           : Center(child: Icon(icon, color: color)),
     );
